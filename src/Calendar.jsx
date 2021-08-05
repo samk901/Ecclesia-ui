26b138354faa60464e54d40fa07c664fa9d45771
samk90 <kim.donghy@northeastern.edu>
2021-08-05 15:25:52 -0400

    changed calendar component

--- src/Calendar.jsx
+++ src/Calendar.jsx
@@ -3,26 +3,6 @@ import store from './store.js';
 import graphQLFetch from './graphQLFetch.js';
 
 export default class Calendar extends React.Component {
-  static async fetchData() {
-    const data = await graphQLFetch('query {about}');
-    return data;
-  }
-
-  constructor(props) {
-    super(props);
-    const apiAbout = store.initialData ? store.initialData.about : null;
-    delete store.initialData;
-    this.state = { apiAbout };
-  }
-
-  async componentDidMount() {
-    const { apiAbout } = this.state;
-    if (apiAbout == null) {
-      const data = await About.fetchData();
-      this.setState({ apiAbout: data.about });
-    }
-  }
-  
   render() {
     return (
       <div>
