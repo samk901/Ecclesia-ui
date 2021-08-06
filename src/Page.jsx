a940fe0b18fa8c5a3df608a6d0bd1c9e844703ed
samk90 <kim.donghy@northeastern.edu>
2021-08-06 12:28:13 -0400

    adjusted navbar style

--- src/Page.jsx
+++ src/Page.jsx
@@ -114,7 +114,7 @@ export default class Page extends React.Component {
     if (user == null) return null;
     return (
       <div>
-        <NavBar user={user} onUserChange={this.onUserChange} />
+        <NavBar bg="dark" user={user} onUserChange={this.onUserChange} />
         <Grid fluid>
           <UserContext.Provider value={user}>
             <Contents />
