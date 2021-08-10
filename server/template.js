7310da57f576e2649e9e07f1c46400ec07f9c3cc
samk90 <kim.donghy@northeastern.edu>
2021-08-10 09:39:08 -0400

    changed template to sandstone.css

--- server/template.js
+++ server/template.js
@@ -6,7 +6,6 @@ export default function template(body, initialData, userData) {
 <head>
   <meta charset="utf-8">
   <title>Ecclessia -- Managing Congregant Care</title>
-  <link rel="stylesheet" href="/bootstrap/css/bootstrap.min.css">
   <link rel="stylesheet" href="/bootstrap/css/sandstone-theme.css">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <script src="https://apis.google.com/js/api:client.js"></script>
