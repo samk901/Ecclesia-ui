0a19c815a36b34ac422524ad37acd916e9a71fea
samk90 <kim.donghy@northeastern.edu>
2021-08-10 10:30:14 -0400

    added inverse to Navbar function in Page

--- src/Page.jsx
+++ src/Page.jsx
@@ -16,7 +16,7 @@ import SignInNavItem from './SignInNavItem.jsx';
 
 function NavBar({ user, onUserChange }) {
   return (
-    <Navbar>
+    <Navbar inverse>
       <Navbar.Header>
         <Navbar.Brand>Ecclesia</Navbar.Brand>
       </Navbar.Header>
